2585501c7e6415e1c5a025c9c5e087f4ecb578a8
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-12-27 02:58:06 +0530

    🔐 Auth with supabase done

--- components/nav/Profile.tsx
+++ components/nav/Profile.tsx
@@ -6,11 +6,27 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { DashboardIcon, LockOpen1Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { createBrowserClient } from "@supabase/ssr";
 
 
 
 export default function Profile(){
     const user = useUser((state)=>state.user);
+    const setUser = useUser((state)=> state.setUser);
+
+    const supabase = createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    const handleLogout = async ()=>{
+        await supabase.auth.signOut();
+        setUser(undefined);
+    }
+
     return(
         <Popover>
             <PopoverTrigger>
@@ -22,7 +38,27 @@ export default function Profile(){
                 className="rounded-full ring-2 ring-green-500"
                 />
             </PopoverTrigger>
-            <PopoverContent>Place content for the popover here.</PopoverContent>
+            <PopoverContent className="space-y-3 divide-y p-2" side="bottom">
+				<div className="px-4">
+					<p className="text-sm">{user?.user_metadata.full_name}</p>
+					<p className="text-sm text-gray-500">{user?.user_metadata.email}</p>
+				</div>
+                <Link href="/dashboard">
+                    <Button
+                        variant="ghost"
+                        className="w-full flex justify-between items-center"
+                    >
+                        Dashboard <DashboardIcon />
+                    </Button>
+                </Link>
+                <Button
+					variant="ghost"
+					className="w-full flex justify-between items-center"
+                    onClick={handleLogout}
+				>
+					Log out <LockOpen1Icon />
+				</Button>
+            </PopoverContent>
         </Popover>
                 
 
